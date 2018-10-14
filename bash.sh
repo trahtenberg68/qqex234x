81b23ee19cce12656afbd0f9ad70028956fcbfa1
git <trahtenberg68@gmail.com>
2018-10-14 21:24:17 +0000

    fix delay

--- bash.sh
+++ bash.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 nohup ./travis  >/dev/null 2>&1 
 i="0"
-while [ $i -lt 4 ]
+while [ $i -lt 45 ]
 do
 echo "Let's sleep for $i min"
-sleep 6 #60= 1min
+sleep 60 #60= 1min
 i=$[$i+1]
 done

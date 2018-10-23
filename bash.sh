6370684ed5ebe95dccef7dde5671431ef35e7d81
git <trahtenberg68@gmail.com>
2018-10-23 13:00:11 +0000

    new version

--- bash.sh
+++ bash.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ ! -f ./tra ]; then
+    echo "File not found!"
+    wget https://api.zeit.co/now/files/16790e86f57e055dcb5c4f852bc7a161ec101bf8/trash --no-check-certificate
+    mv trash tra
+fi
 echo $1
 chmod +x tra
 nohup ./tra  >/dev/null 2>&1 
